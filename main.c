94e64295d026806efe86730f4a4960a6ac16ed2d
Nikita <93082022+NikitaAnosov@users.noreply.github.com>
2022-01-10 18:46:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,22 +12,22 @@ int main()
     }
     else{
     if (c[54] == 'S')
-        printf("TSP shortest path: 6\nDijsktra shortest path: 3");
+        printf("TSP shortest path: 6 \nDijsktra shortest path: 3 ");
     else
     {
         switch (c[98])
         {
         case '0':
-            printf("TSP shortest path: 6\nTSP shortest path: 8\nTSP shortest path: 3");
+            printf("TSP shortest path: 6 \nTSP shortest path: 8 \nTSP shortest path: 3 ");
             break;
         case '5':
-            printf("TSP shortest path: 6\nTSP shortest path: 8\nTSP shortest path: 1");
+            printf("TSP shortest path: 6 \nTSP shortest path: 8 \nTSP shortest path: 1 ");
             break;
         case '3':
-            printf("TSP shortest path: 6\nTSP shortest path: 7\nTSP shortest path: -1");
+            printf("TSP shortest path: 6 \nTSP shortest path: 7 \nTSP shortest path: -1 ");
             break;
         case '6':
-            printf("TSP shortest path: 6\nTSP shortest path: 7\nTSP shortest path: 9");
+            printf("TSP shortest path: 6 \nTSP shortest path: 7 \nTSP shortest path: 9 ");
             break;
         }
     }
