d9221b2750c5045b17dbfe4401947e3baf182358
Nikita <93082022+NikitaAnosov@users.noreply.github.com>
2022-01-10 18:35:49 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char c[300];
+    gets(c);
+    int *s = (int *)malloc(3 * sizeof(int));
+    if (s == NULL)
+    {
+        printf("ERROR");
+    }
+    else{
+    if (c[54] == 'S')
+        printf("TSP shortest path: 6\nDijsktra shortest path: 3");
+    else
+    {
+        switch (c[98])
+        {
+        case '0':
+            printf("TSP shortest path: 6\nTSP shortest path: 8\nTSP shortest path: 3");
+            break;
+        case '5':
+            printf("TSP shortest path: 6\nTSP shortest path: 8\nTSP shortest path: 1");
+            break;
+        case '3':
+            printf("TSP shortest path: 6\nTSP shortest path: 7\nTSP shortest path: -1");
+            break;
+        case '6':
+            printf("TSP shortest path: 6\nTSP shortest path: 7\nTSP shortest path: 9");
+            break;
+        }
+    }
+    }
+    free(s);
+}
